414dd162aafe54567184524158ee146aeac78d7c
Rui Marinho <me@ruimarinho.net>
2016-10-16 23:52:14 +0100

    fix references project and add sharpie script

---
+++ Xamarin/AppRTC/AppRTC/parse.sh
@@ -0,0 +1,16 @@
+sharpie bind -o ../sharpie -sdk iphoneos10.0 \
+AppRTC/*.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCPeerConnection.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCPeerConnectionDelegate.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCPeerConnectionFactory.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCPeerConnectionInterface.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCMediaStream.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCAudioTrack.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCStatsDelegate.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCDataChannel.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCVideoSource.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCVideoTrack.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCVideoCapturer.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCVideoRenderer.h \
+ExtraLibs/libjingle_peerconnection/Headers/RTCI420Frame.h \
+-c -IExtraLibs/libjingle_peerconnection/Headers/
